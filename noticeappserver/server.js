dbc73f4271599bcb2ebea98a201f0271e7525590
Juri Baier <juri.b@hotmail.com>
2023-03-29 09:38:06 +0200

    adds first server files

---
+++ noticeappserver/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+
+app.listen(9000, () => {
+    console.log("Server listening now on Port 9000");
+})
+
+app.get("/", (req, res) => {
+    res.send("Test");
+})
